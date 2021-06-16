22ec2f187734265b9e47a0df612a48788be1217f
Yasin Mogultay <yasin.mglty@gmail.com>
2021-06-16 16:55:44 -0500

    completed first round of intro questions

--- CoreJava.java
+++ CoreJava.java
@@ -13,4 +13,21 @@ public class CoreJava {
         return (int)Math.pow(10,n)-1;
     }
 
+    public static int candies(int n, int m) {
+        int div = m / n;
+        return div * n;
+    }
+
+    public static int seatsInTheater(int nCols, int nRows, int col, int row) {
+        int n1 =(nCols - col) + 1;
+        int n2 =nRows - row;
+        return n1 * n2;
+    }
+
+    public static int maxMultiple(int divisor, int bound) {
+        return (bound / divisor) * divisor;
+    }
+
+
+
 }
