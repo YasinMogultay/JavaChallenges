7ff0b505c0b942de971dca8e82964eb61d63f7f9
Yasin Mogultay <yasin.mglty@gmail.com>
2021-06-17 14:56:29 -0500

    feat:completed more java questions

--- CoreJava.java
+++ CoreJava.java
@@ -28,6 +28,40 @@ public class CoreJava {
         return (bound / divisor) * divisor;
     }
 
+    public static boolean reachNextLevel(int experience, int threshold, int reward) {
+        if((experience + reward ) < threshold){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+        if (weight1 + weight2 <= maxW)
+            return value1 + value2;
+        else if (weight1 > maxW && weight2 > maxW)
+            return 0;
+        else if (weight1 <= maxW && weight2 > maxW)
+            return value1;
+        else if (weight2 <= maxW && weight1 > maxW)
+            return value2;
+        else if (value1 >= value2)
+            return value1;
+        else if (value1 < value2)
+            return value2;
+
+        return 0;
+    }
+
+    public static int extraNumber(int a, int b, int c) {
+        if(a == b){
+            return c;
+        } else if (a == c) {
+            return b;
+        } else {
+            return a;
+        }
+    }
 
 
 }
