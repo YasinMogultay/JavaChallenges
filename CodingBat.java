af22f2e3fa886fd496f0b431d2c43156e4865723
Yasin Mogultay <yasin.mglty@gmail.com>
2021-07-08 19:15:34 -0500

    more java challenges

--- CodingBat.java
+++ CodingBat.java
@@ -71,7 +71,7 @@ public class CodingBat {
 
     //Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
     public String notString(String str) {
-        if(str.length() >= 3 && str.substring(0,3).equals("not")){
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
             return str;
         }
         return "not " + str;
@@ -83,7 +83,103 @@ public class CodingBat {
         return sb.deleteCharAt(n).toString();
     }
 
+    //Given a string, return a new string where the first and last chars have been exchanged.
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
 
+        char first = str.charAt(0);
+        String mid = str.substring(1, str.length() - 1);
+        char last = str.charAt(str.length() - 1);
+        return first + mid + last;
+    }
+
+    //Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+    public String front3(String str) {
+        if (str.length() >= 3) {
+            String firstThree = str.substring(0, 3);
+            return firstThree + firstThree + firstThree;
+        } else if (str.length() == 2) {
+            String firstThree = str.substring(0, 2);
+            return firstThree + firstThree + firstThree;
+        } else if (str.length() == 1) {
+            String firstThree = str.substring(0, 1);
+            return firstThree + firstThree + firstThree;
+        } else {
+            return str;
+        }
+    }
+
+    //Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+    public String backAround(String str) {
+        char last = str.charAt(str.length() - 1);
+        return last + str + last;
+    }
+
+    //Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+    public boolean or35(int n) {
+        if (n % 3 == 0 || n % 5 == 0)
+            return true;
+        return false;
+    }
+
+    //Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there
+    public String front22(String str) {
+        int take = 2;
+        if (take > str.length()) {
+            take = str.length();
+        }
+        String front = str.substring(0, take);
+        return front + str + front;
+    }
+
+    //Given a string, return true if the string starts with "hi" and false otherwise.
+    public boolean startHi(String str) {
+        if (str.length() >= 2 && str.substring(0, 2).equals("hi"))
+            return true;
+        return false;
+    }
+
+    //Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+    public boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    //We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+    public boolean hasTeen(int a, int b, int c) {
+        if ((a <= 19 && a >= 13) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19))
+            return true;
+        return false;
+
+    }
+
+    //Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del"))
+            return str.substring(0, 1) + str.substring(4, str.length());
+        return str;
+    }
+
+    //Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+    public boolean mixStart(String str) {
+        if (str.length() >= 3 && str.substring(1, 3).equals("ix"))
+            return true;
+        return false;
+    }
+
+    //Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+    public String startOz(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0) == 'o') {
+            result = result + str.charAt(0);
+        }
+        if (str.length() >= 2 && str.charAt(1) == 'z') {
+            result = result + str.charAt(1);
+        }
+        return result;
+    }
 
 
 }
