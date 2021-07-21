5c349a033d646eda06e695be17442bd5a2c4841d
Yasin Mogultay <yasin.mglty@gmail.com>
2021-07-21 17:44:42 -0500

    leet code java challenges

--- LeetCode.java
+++ LeetCode.java
@@ -190,6 +190,17 @@ public class LeetCode {
 //        reverseList(arrayList);
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+
 
 }
 
