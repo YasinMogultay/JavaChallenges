5e284b47f09a58e3f11c71fd617983623ed007c0
Yasin Mogultay <yasin.mglty@gmail.com>
2021-07-23 20:06:19 -0500

    adjusted

--- BenefitFocusCodingChallenge.java
+++ BenefitFocusCodingChallenge.java
@@ -1,17 +1,17 @@
 public class BenefitFocusCodingChallenge {
     public static final int SIZE = 26;//alphabet size
 
-    public static void printLetterFrequency(String str){
+    public static void printLetterFrequency(String str) {
         int n = str.length();//size of string
         int[] freq = new int[SIZE];
 
         //frequency each char in given string
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             freq[str.charAt(i) - 'a']++;
         }
         //if frequency != 0 print char along with frequency
-        for(int i=0; i<n; i++){
-            if(freq[str.charAt(i) - 'a'] != 0){
+        for (int i = 0; i < n; i++) {
+            if (freq[str.charAt(i) - 'a'] != 0) {
                 System.out.print(str.charAt(i));
                 System.out.print(freq[str.charAt(i) - 'a']);
                 //update freq of char to 0 to avoid print same char again
