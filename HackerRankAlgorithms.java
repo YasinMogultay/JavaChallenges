3d10671db6788a8a1d1f594161008cd9e1336cea
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-03 12:16:12 -0500

    fixed miniMaxSum solution and solved birthdayCakeCandles problem

--- HackerRankAlgorithms.java
+++ HackerRankAlgorithms.java
@@ -12,22 +12,32 @@ public class HackerRankAlgorithms {
      */
 
     public static void miniMaxSum(List<Integer> arr) {
-        // Write your code here
-        Collections.sort(arr);
-        int minTotal = 0;
-        int maxTotal = 0;
-        int length = arr.size() - 1;
-        for (int i = 0; i < length; i++) {
-            minTotal += arr.get(i);
-            maxTotal += arr.get(length - i);
+        int min = arr.get(0);
+        int max = arr.get(0);
+        long totalSum=0;
+        for (int n : arr) {
+            totalSum += n;
+            if (n < min) min = n;
+            if (n > max) max = n;
         }
-        System.out.print(minTotal + " " + maxTotal);
+        System.out.println((totalSum-max) + " " + (totalSum-min));
+    }
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        Collections.sort(candles);
+        int max = candles.get(candles.size()-1);
+        return Collections.frequency( candles, max);
     }
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(7, 69, 2, 221, 8974);
-//        Collections.sort(list);
-//        System.out.println(list);
-        miniMaxSum(list);
+        List<Integer> array = Arrays.asList(3, 2, 1, 3);
+        System.out.println(birthdayCakeCandles(array));
     }
 }
