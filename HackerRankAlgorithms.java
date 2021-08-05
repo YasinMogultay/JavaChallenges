fe10afe05c9a5503df8e07f8b3ebba13610629e0
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-05 12:49:58 -0500

    completed reverseArray

--- HackerRankAlgorithms.java
+++ HackerRankAlgorithms.java
@@ -120,5 +120,22 @@ public class HackerRankAlgorithms {
         return resGrades;
     }
 
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+        // Write your code here
+        List<Integer> newArray = new ArrayList<Integer>(a.size());
+        for(int i = a.size()-1; i >= 0;  i--) {
+            newArray.add(a.get(i));
+        }
+        return newArray;
+    }
+
+
 
 }
