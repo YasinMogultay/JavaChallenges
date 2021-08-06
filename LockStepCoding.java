d63ad6b8994da733b348c3607327b6f7908e377c
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-06 12:40:20 -0500

    solved second one

--- LockStepCoding.java
+++ LockStepCoding.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LockStepCoding {
     //first tutorial question about page
     public static int solve(int weight0, int weight1, int weight2) {
@@ -10,6 +12,21 @@ public class LockStepCoding {
         } else {
             return 2;
         }
+    }
+
+    public static int computeClosestToZero(int[] ts) {
+        // Write your code here
+        // To debug: System.err.println("Debug messages...");
+        Arrays.sort(ts);
+        int curr = 0;
+        int near = ts[0];
+        for (int i = 0; i < ts.length; i++) {
+            curr = ts[i] * ts[i];
+            if (curr <= (near * near)) {
+                near = ts[i];
+            }
+        }
+        return near;
     }
 
 }
