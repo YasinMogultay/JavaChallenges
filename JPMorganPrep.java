3aee4a72a63f41a64500eb19687c09a0a2c4fc9c
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-06 16:48:36 -0500

    solved isValid one

--- JPMorganPrep.java
+++ JPMorganPrep.java
@@ -1,14 +1,28 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class JPMorganPrep {
     public boolean isValid(String s) {
         if (s.length() % 2 != 0) return false;
-        List<Character> open = Arrays.asList('(','{','[');
-        List<Character> close = Arrays.asList(')','}','[',']');
-        for (char i : s.toCharArray()){
 
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            //put any opening parant to stack
+            //then check for a closing type and remove stack
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }else {
+                return false;
+            }
         }
+        return stack.isEmpty();
     }
 }
