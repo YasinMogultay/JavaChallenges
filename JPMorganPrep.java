f09390d318fce5bda0237530c02f1761ca62bef7
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-08 22:38:56 -0500

    another problem solved

--- JPMorganPrep.java
+++ JPMorganPrep.java
@@ -8,21 +8,44 @@ public class JPMorganPrep {
         if (s.length() % 2 != 0) return false;
 
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()){
+        for (char c : s.toCharArray()) {
             //put any opening parant to stack
             //then check for a closing type and remove stack
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            }else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
                 stack.pop();
-            }else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
                 stack.pop();
-            }else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
                 stack.pop();
-            }else {
+            } else {
                 return false;
             }
         }
         return stack.isEmpty();
+    }
+
+
+    public static boolean isSelfDescribing(long a) {
+        String s = String.valueOf(a);
+        for (int i = 0; i < s.length(); i++) {
+            int b = s.charAt(i) - '0'; // number of times i-th digit must occur for it to be a self describing number
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                int temp = s.charAt(j) - '0';
+                if (temp == i) {
+                    count++;
+                }
+                if (count > b) return false;
+            }
+            if (count != b) return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isSelfDescribing(2020));
     }
 }
--- LockStepCoding.java
+++ LockStepCoding.java
@@ -29,4 +29,8 @@ public class LockStepCoding {
         return near;
     }
 
+
+
+
+
 }
