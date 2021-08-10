b115612a2f324af7fbeb3e158517af444e8f34e7
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-09 20:28:53 -0500

    solved fibonacci in leetcode

--- JPMorganPrep.java
+++ JPMorganPrep.java
@@ -84,6 +84,25 @@ public class JPMorganPrep {
         return memo[0];
     }
 
+    public int fib(int n) {
+        if(n <= 1) return n;
+
+        //for the nth Fibonacci we only need n - 1 and n - 2 element
+        //here we take 2 variable
+        //last and secondLast
+
+        int last = 1;
+        int secondLast = 0;
+
+        for(int i = 2; i <= n; i++){
+            int sum = last + secondLast;
+
+            secondLast = last;
+            last = sum;
+        }
+
+        return last;
+    }
 
     public static void main(String[] args) {
         System.out.println(isSelfDN2(2020));
