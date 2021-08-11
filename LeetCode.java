b09f82e0cfd21961cf19daf10208fc8d65927214
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-11 16:08:42 -0500

    solved maximum sub array question on leet code

--- LeetCode.java
+++ LeetCode.java
@@ -258,5 +258,45 @@ public class LeetCode {
         if(end>0 &&nums[end]==val) count++;
         return nums.length-count;
     }
+
+
+//    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+    public int maxSubArray(int[] nums) {
+        return Subarray(nums, 0 ,nums.length -1 );
+    }
+    public int Subarray(int[] A,int left, int right){
+        if(left == right){return A[left];}
+        int mid = left + (right - left) / 2;
+        int leftSum = Subarray(A,left,mid);// left part
+        int rightSum = Subarray(A,mid+1,right);//right part
+        int crossSum = crossSubarray(A,left,right);// cross part
+        if(leftSum >= rightSum && leftSum >= crossSum){// left part is max
+            return leftSum;
+        }
+        if(rightSum >= leftSum && rightSum >= crossSum){// right part is max
+            return rightSum;
+        }
+        return crossSum; // cross part is max
+    }
+    public int crossSubarray(int[] A,int left,int right){
+        int leftSum = Integer.MIN_VALUE;
+        int rightSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int mid = left + (right - left) / 2;
+        for(int i = mid; i >= left ; i--){
+            sum = sum + A[i];
+            if(leftSum < sum){
+                leftSum = sum;
+            }
+        }
+        sum = 0;
+        for(int j = mid + 1; j <= right; j++){
+            sum = sum + A[j];
+            if(rightSum < sum){
+                rightSum = sum;
+            }
+        }
+        return leftSum + rightSum;
+    }
 }
 
