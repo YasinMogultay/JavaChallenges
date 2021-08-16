182d630da52f6ec91f7642ab301cb972ae218f56
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-16 16:48:02 -0500

    completed maxArea question on leetcode

--- LeetCode.java
+++ LeetCode.java
@@ -426,5 +426,28 @@ public class LeetCode {
             maxLen = k - j - 1;
         }
     }
+
+    //11. Container With Most Water
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+
+        int ans=0;
+        while(i!=j){
+
+            int area=(j-i)*Math.min(height[i],height[j]);
+
+            if(area>ans){
+                ans=area;
+            }
+
+            if(height[i]>height[j]){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return ans;
+    }
 }
 
