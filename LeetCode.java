ef6f7391e898b1587fe071ff455feddaa3c7bd30
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-16 16:50:01 -0500

    solved threeSumClosest question on leetcode

--- LeetCode.java
+++ LeetCode.java
@@ -449,5 +449,28 @@ public class LeetCode {
         }
         return ans;
     }
+
+
+    //Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+    public int threeSumClosest(int[] num, int target) {
+        int result = num[0] + num[1] + num[num.length - 1];
+        Arrays.sort(num);
+        for (int i = 0; i < num.length - 2; i++) {
+            int start = i + 1, end = num.length - 1;
+            while (start < end) {
+                int sum = num[i] + num[start] + num[end];
+                if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
 }
 
