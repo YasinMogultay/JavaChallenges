ad9a62a63762fd10feb1489f142df0039a4232ee
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-18 19:25:11 -0500

    solved divide two integer questio on leetcode

--- LeetCode.java
+++ LeetCode.java
@@ -513,5 +513,17 @@ public class LeetCode {
         }
         return -1;
     }
+
+    //Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+    public int divide(int A, int B) {
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A), b = Math.abs(B), res = 0, x = 0;
+        while (a - b >= 0) {
+            for (x = 0; a - (b << x << 1) >= 0; x++);
+            res += 1 << x;
+            a -= b << x;
+        }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
 }
 
