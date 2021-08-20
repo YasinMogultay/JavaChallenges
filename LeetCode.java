b9576180c4cd84b6a8f89b8694bad7bf29c6e1b8
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-19 20:23:48 -0500

    solved generate parantheses question

--- LeetCode.java
+++ LeetCode.java
@@ -525,5 +525,25 @@ public class LeetCode {
         }
         return (A > 0) == (B > 0) ? res : -res;
     }
+
+//    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
 }
 
