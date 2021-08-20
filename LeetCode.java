a1607debf740c63450e9bd0b3f8996b606c0b67b
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-19 20:27:06 -0500

    solved first and last position of element in sorted array

--- LeetCode.java
+++ LeetCode.java
@@ -545,5 +545,38 @@ public class LeetCode {
         if(close < open)
             backtrack(list, str+")", open, close+1, max);
     }
+
+//    Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+    public int[] searchRange(int[] A, int target) {
+        int start = Solution.firstGreaterEqual(A, target);
+        if (start == A.length || A[start] != target) {
+            return new int[]{-1, -1};
+        }
+        return new int[]{start, Solution.firstGreaterEqual(A, target + 1) - 1};
+    }
+
+    //find the first number that is greater than or equal to target.
+    //could return A.length if target is greater than A[A.length-1].
+    //actually this is the same as lower_bound in C++ STL.
+    private static int firstGreaterEqual(int[] A, int target) {
+        int low = 0, high = A.length;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            //low <= mid < high
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else {
+                //should not be mid-1 when A[mid]==target.
+                //could be mid even if A[mid]>target because mid<high.
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+
+
+
+
 }
 
