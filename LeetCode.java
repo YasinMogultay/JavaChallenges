53ca4ed286d65d9cf6f46c5d980ae5daa303d503
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-23 12:14:29 -0500

    completed multiply strings questions

--- LeetCode.java
+++ LeetCode.java
@@ -670,5 +670,27 @@ public class LeetCode {
         // 4. no positive numbers were found, which means the array contains all numbers 1..n
         return n + 1;
     }
+
+
+//    Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
+
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
 }
 
