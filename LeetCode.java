b8772e832ff61f7df65cf33446df7b8c76469243
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-24 11:29:13 -0500

    solved permutations question on leetcode

--- LeetCode.java
+++ LeetCode.java
@@ -692,5 +692,32 @@ public class LeetCode {
         for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
         return sb.length() == 0 ? "0" : sb.toString();
     }
+
+
+    //Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        if (nums.length == 0) {
+            return permutations;
+        }
+
+        collectPermutations(nums, 0, new ArrayList<>(), permutations);
+        return permutations;
+    }
+
+    private void collectPermutations(int[] nums, int start, List<Integer> permutation,
+                                     List<List<Integer>>  permutations) {
+
+        if (permutation.size() == nums.length) {
+            permutations.add(permutation);
+            return;
+        }
+
+        for (int i = 0; i <= permutation.size(); i++) {
+            List<Integer> newPermutation = new ArrayList<>(permutation);
+            newPermutation.add(i, nums[start]);
+            collectPermutations(nums, start + 1, newPermutation, permutations);
+        }
+    }
 }
 
