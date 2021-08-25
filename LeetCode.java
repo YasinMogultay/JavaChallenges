d8b74f415ac55c5d8b013975391b3d631e4c46cc
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-25 12:36:06 -0500

    daily leetcode problme solved

--- LeetCode.java
+++ LeetCode.java
@@ -745,5 +745,15 @@ public class LeetCode {
         return s1;
     }
 
+    //Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+    public double pow(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+    }
 }
 
