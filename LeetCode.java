928caea4b8969627ea4f27cb669f682dd96f68a2
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-25 13:27:24 -0500

    solved jump game in leetcode

--- LeetCode.java
+++ LeetCode.java
@@ -764,5 +764,24 @@ public class LeetCode {
         }
         return res;
     }
+
+
+//    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+//    Return true if you can reach the last index, or false otherwise.
+    public boolean canJump(int[] nums) {
+        if(nums.length < 2) return true;
+
+        for(int curr = nums.length-2; curr>=0;curr--){
+            if(nums[curr] == 0){
+                int neededJumps = 1;
+                while(neededJumps > nums[curr]){
+                    neededJumps++;
+                    curr--;
+                    if(curr < 0) return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
