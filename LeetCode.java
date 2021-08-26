5a70987a47934aeec983bb7ff92711ab541ed295
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-26 18:51:19 -0500

    solved length of last world problem on leet code

--- LeetCode.java
+++ LeetCode.java
@@ -783,5 +783,22 @@ public class LeetCode {
         }
         return true;
     }
+
+//    Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+//    A word is a maximal substring consisting of non-space characters only.
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+
+        // We are looking for the last word so let's go backward
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') { // a letter is found so count
+                length++;
+            } else {  // it's a white space instead
+                //  Did we already started to count a word ? Yes so we found the last word
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
 }
 
