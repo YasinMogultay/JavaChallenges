15fdccee15e10dc014fcb10510001fd326a83f0f
Yasin Mogultay <yasin.mglty@gmail.com>
2021-08-27 19:58:58 -0500

    solved merge arrays

--- LeetCode.java
+++ LeetCode.java
@@ -800,5 +800,19 @@ public class LeetCode {
         }
         return length;
     }
+
+//    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+//   representing the number of elements in nums1 and nums2 respectively.
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ?
+                    nums1[tail1--] : nums2[tail2--];
+        }
+
+        while (tail2 >= 0) { //only need to combine with remaining nums2
+            nums1[finished--] = nums2[tail2--];
+        }
+    }
 }
 
