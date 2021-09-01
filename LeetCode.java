81ef46c2ebb8fe7e55a321d245c6edb712ef9a3a
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-01 18:48:44 -0500

    solved generateMatrix question

--- LeetCode.java
+++ LeetCode.java
@@ -913,5 +913,32 @@ public int jump(int[] N) {
     }
     return ans;
 }
+
+//Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+public static int[][] generateMatrix(int n) {
+    int[][] ret = new int[n][n];
+    int left = 0,top = 0;
+    int right = n -1,down = n - 1;
+    int count = 1;
+    while (left <= right) {
+        for (int j = left; j <= right; j ++) {
+            ret[top][j] = count++;
+        }
+        top ++;
+        for (int i = top; i <= down; i ++) {
+            ret[i][right] = count ++;
+        }
+        right --;
+        for (int j = right; j >= left; j --) {
+            ret[down][j] = count ++;
+        }
+        down --;
+        for (int i = down; i >= top; i --) {
+            ret[i][left] = count ++;
+        }
+        left ++;
+    }
+    return ret;
+}
 }
 
