583e29a1d167f6cde1c2e13b9499bf797cb806a8
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-02 18:48:21 -0500

    solved reverse polish notation problem

--- LeetCode.java
+++ LeetCode.java
@@ -978,5 +978,39 @@ public class LeetCode {
         }
         return dp[s.length()];
     }
+
+
+//    Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+//Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+//Note that division between two integers should truncate toward zero.
+//It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+    public int evalRPN(String[] tokens) {
+        int a,b;
+        Stack<Integer> S = new Stack<Integer>();
+        for (String s : tokens) {
+            if(s.equals("+")) {
+                S.add(S.pop()+S.pop());
+            }
+            else if(s.equals("/")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a / b);
+            }
+            else if(s.equals("*")) {
+                S.add(S.pop() * S.pop());
+            }
+            else if(s.equals("-")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a - b);
+            }
+            else {
+                S.add(Integer.parseInt(s));
+            }
+        }
+        return S.pop();
+    }
+
 }
 
