25c06c828d92f6d197422a5abfd898bd85fe4f82
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-03 16:14:46 -0500

    solved gas station question on leet code

--- LeetCode.java
+++ LeetCode.java
@@ -1037,5 +1037,28 @@ public class LeetCode {
         return max;
     }
 
+//There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i]. You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+//Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tank = 0;
+        for(int i = 0; i < gas.length; i++)
+            tank += gas[i] - cost[i];
+        if(tank < 0)
+            return - 1;
+
+        int start = 0;
+        int accumulate = 0;
+        for(int i = 0; i < gas.length; i++){
+            int curGain = gas[i] - cost[i];
+            if(accumulate + curGain < 0){
+                start = i + 1;
+                accumulate = 0;
+            }
+            else accumulate += curGain;
+        }
+
+        return start;
+    }
+
 }
 
