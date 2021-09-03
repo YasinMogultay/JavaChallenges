5f25d8641addeacda8a142edf7cf38b75923554c
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-03 16:12:09 -0500

    solved longest consecutive sequence

--- LeetCode.java
+++ LeetCode.java
@@ -1012,5 +1012,30 @@ public class LeetCode {
         return S.pop();
     }
 
+//    Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int num: nums) set.add(num);
+        int max = 1;
+        for(int num: nums) {
+            if(set.remove(num)) {//num hasn't been visited
+                int val = num;
+                int sum = 1;
+                while(set.remove(val-1)) val--;
+                sum += num - val;
+
+                val = num;
+                while(set.remove(val+1)) val++;
+                sum += val - num;
+
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+
 }
 
