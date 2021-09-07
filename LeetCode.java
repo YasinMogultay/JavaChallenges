4be6248a030714fd84822aec5cf644171b61f700
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-07 13:40:42 -0500

    solved max product question on leet code

--- LeetCode.java
+++ LeetCode.java
@@ -1114,5 +1114,25 @@ public class LeetCode {
         }
     }
 
+
+
+//    Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+//    It is guaranteed that the answer will fit in a 32-bit integer.
+//    A subarray is a contiguous subsequence of the array.
+    public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
 }
 
