fbee9adb7cf6793e24cd2c567d2eca57bfb79e28
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-08 20:07:16 -0500

    permutation sequence question solved

--- LeetCode.java
+++ LeetCode.java
@@ -1196,5 +1196,27 @@ public class LeetCode {
             return res;
         }
 
+
+
+//The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+//    By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+    public String getPermutation(int n, int k) {
+        List<Integer> num = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) num.add(i);
+        int[] fact = new int[n];  // factorial
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) fact[i] = i*fact[i-1];
+        k = k-1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--){
+            int ind = k/fact[i-1];
+            k = k%fact[i-1];
+            sb.append(num.get(ind));
+            num.remove(ind);
+        }
+        return sb.toString();
+    }
+
+
 }
 
