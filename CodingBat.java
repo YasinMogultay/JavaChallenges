55d2e7a2d5bc31a731345d8794b6994cee868646
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-10 20:31:51 -0500

    solved some more coding bat questions

--- CodingBat.java
+++ CodingBat.java
@@ -261,6 +261,27 @@ public class CodingBat {
         return result;
     }
 
+//    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+    }
 
+//Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+public String frontTimes(String str, int n) {
+    int frontLen = 3;
+    if (frontLen > str.length()) {
+        frontLen = str.length();
+    }
+    String front = str.substring(0, frontLen);
 
+    String result = "";
+    for (int i=0; i<n; i++) {
+        result = result + front;
+    }
+    return result;
+}
 }
