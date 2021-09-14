205d212359ffdf818fbf3b42f0f365910a11eb99
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-13 20:12:44 -0500

    solved some warmup challenges on codebat

--- CodingBat.java
+++ CodingBat.java
@@ -284,4 +284,28 @@ public String frontTimes(String str, int n) {
     }
     return result;
 }
+
+//    Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+    int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str.substring(i, i+2).equals("xx")) count++;
+        }
+        return count;
+    }
+
+//    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) return false; // no "x" at all
+
+        // Is char at i+1 also an "x"?
+        if (i+1 >= str.length()) return false; // check i+1 in bounds?
+        return str.substring(i+1, i+2).equals("x");
+
+        // Another approach -- .startsWith() simplifies the logic
+        // String x = str.substring(i);
+        // return x.startsWith("xx");
+    }
+
 }
