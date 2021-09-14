a0d2af18c57eaa9161b050fc03efdaf37842c3a9
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-13 20:14:20 -0500

    solved some more challenges

--- CodingBat.java
+++ CodingBat.java
@@ -308,4 +308,45 @@ public String frontTimes(String str, int n) {
         // return x.startsWith("xx");
     }
 
+
+//    Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+    public String stringBits(String str) {
+        String result = "";
+        // Note: the loop increments i by 2
+        for (int i=0; i<str.length(); i+=2) {
+            result = result + str.substring(i, i+1);
+            // Alternately could use str.charAt(i)
+        }
+        return result;
+    }
+
+//    Given a non-empty string like "Code" return a string like "CCoCodCode".
+    public String stringSplosion(String str) {
+        String result = "";
+        // On each iteration, add the substring of the chars 0..i
+        for (int i=0; i<str.length(); i++) {
+            result = result + str.substring(0, i+1);
+        }
+        return result;
+    }
+
+//    Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+    public int last2(String str) {
+        // Screen out too-short string case.
+        if (str.length() < 2) return 0;
+
+        String end = str.substring(str.length()-2);
+        // Note: substring() with 1 value goes through the end of the string
+        int count = 0;
+
+        // Check each substring length 2 starting at i
+        for (int i=0; i<str.length()-2; i++) {
+            String sub = str.substring(i, i+2);
+            if (sub.equals(end)) {  // Use .equals() with strings
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
