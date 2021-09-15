ef489999435a3db080989fee565b641e201efda1
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-14 21:49:15 -0500

    solved more questions

--- CodingBat.java
+++ CodingBat.java
@@ -373,4 +373,30 @@ public String frontTimes(String str, int n) {
 
         return false;
     }
+
+    // Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+    public boolean array123(int[] nums) {
+        // Note: iterate < length-2, so can use i+1 and i+2 in the loop
+        for (int i=0; i < (nums.length-2); i++) {
+            if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3) return true;
+        }
+        return false;
+    }
+
+    // Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+    public int stringMatch(String a, String b) {
+
+        int len = Math.min(a.length(), b.length());
+        int count = 0;
+
+        for (int i=0; i<len-1; i++) {
+            String aSub = a.substring(i, i+2);
+            String bSub = b.substring(i, i+2);
+            if (aSub.equals(bSub)) {  // Use .equals() with strings
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
