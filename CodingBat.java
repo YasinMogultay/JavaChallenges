a95ba5ab7cb0f9d800531835ed387d9f3e904641
Yasin Mogultay <yasin.mglty@gmail.com>
2021-09-24 19:48:43 -0500

    solved arrays

--- CodingBat.java
+++ CodingBat.java
@@ -492,39 +492,40 @@ public class CodingBat {
         return false;
     }
 
-//    Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+    //    Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
     public boolean firstLast6(int[] nums) {
         if (nums[0] == 6 || nums[nums.length - 1] == 6)
             return true;
         return false;
     }
 
-//    Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+    //    Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
     public boolean sameFirstLast(int[] nums) {
-        if(nums.length == 1)
+        if (nums.length == 1)
             return true;
-        if (nums.length > 1 && nums[0] == nums[nums.length-1])
+        if (nums.length > 1 && nums[0] == nums[nums.length - 1])
             return true;
         return false;
     }
 
-//    Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
-public boolean commonEnd(int[] a, int[] b) {
-    return (a[0] == b[0] ||
-            a[a.length -1] == b[b.length -1] );
-}
+    //    Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+    public boolean commonEnd(int[] a, int[] b) {
+        return (a[0] == b[0] ||
+                a[a.length - 1] == b[b.length - 1]);
+    }
 
-//Given an array of ints length 3, return the sum of all the elements.
+    //Given an array of ints length 3, return the sum of all the elements.
     public int sum3(int[] nums) {
         return nums[0] + nums[1] + nums[2];
     }
-//Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+    //Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
     public int[] rotateLeft3(int[] nums) {
 
-        return new int[] {nums[1],nums[2],nums[0]};
+        return new int[]{nums[1], nums[2], nums[0]};
     }
 
-//    Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+    //    Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
     public int[] reverse3(int[] nums) {
         int[] myArray = new int[3];
 
@@ -534,22 +535,21 @@ public boolean commonEnd(int[] a, int[] b) {
         return myArray;
     }
 
-//    Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+    //    Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
     public int[] maxEnd3(int[] nums) {
-        if (nums[0] >= nums[nums.length-1]) {
+        if (nums[0] >= nums[nums.length - 1]) {
             nums[0] = nums[0];
             nums[1] = nums[0];
             nums[2] = nums[0];
+        } else if (nums[0] <= nums[nums.length - 1]) {
+            nums[0] = nums[nums.length - 1];
+            nums[1] = nums[nums.length - 1];
+            nums[2] = nums[nums.length - 1];
         }
-        else if (nums[0] <= nums[nums.length-1]) {
-            nums[0] = nums[nums.length-1];
-            nums[1] = nums[nums.length-1];
-            nums[2] = nums[nums.length-1];
-        }
-        return new int[] { nums[0],nums[1],nums[2]};
+        return new int[]{nums[0], nums[1], nums[2]};
     }
 
-//    Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+    //    Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
     public int sum2(int[] nums) {
         if (nums.length == 1)
             return nums[0];
@@ -559,18 +559,18 @@ public boolean commonEnd(int[] a, int[] b) {
             return nums[0] + nums[1];
     }
 
-//    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+    //    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
     public int[] middleWay(int[] a, int[] b) {
-        int[] myArray = new int[] {a[1], b[1]};
+        int[] myArray = new int[]{a[1], b[1]};
         return myArray;
     }
 
     // Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
     public int[] makeEnds(int[] nums) {
-        return new int[] {nums[0],nums[nums.length-1]};
+        return new int[]{nums[0], nums[nums.length - 1]};
     }
 
-//    Given an int array length 2, return true if it contains a 2 or a 3.
+    //    Given an int array length 2, return true if it contains a 2 or a 3.
     public boolean has23(int[] nums) {
         if (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3)
             return true;
@@ -578,7 +578,7 @@ public boolean commonEnd(int[] a, int[] b) {
             return false;
     }
 
-//    Given an int array length 2, return true if it does not contain a 2 or 3.
+    //    Given an int array length 2, return true if it does not contain a 2 or 3.
     public boolean no23(int[] nums) {
         if (nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3)
             return true;
@@ -586,14 +586,14 @@ public boolean commonEnd(int[] a, int[] b) {
             return false;
     }
 
-//Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more.
+    //Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more.
     public int[] makeLast(int[] nums) {
-        int[] num = new int[nums.length*2];
-        num[nums.length*2 - 1] = nums[nums.length -1];
+        int[] num = new int[nums.length * 2];
+        num[nums.length * 2 - 1] = nums[nums.length - 1];
         return num;
     }
 
-//    Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+    //    Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
     public boolean double23(int[] nums) {
         if (nums.length < 2)
             return false;
@@ -603,27 +603,29 @@ public boolean commonEnd(int[] a, int[] b) {
             return false;
     }
 
-//    Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
+    //    Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
     public int[] fix23(int[] nums) {
         if (nums[0] == 2 && nums[1] == 3)
             nums[1] = 0;
         if (nums[1] == 2 && nums[2] == 3)
             nums[2] = 0;
-        return new int[] {nums[0],nums[1],nums[2]};
+        return new int[]{nums[0], nums[1], nums[2]};
     }
 
-//    Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+    //    Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
     public int start1(int[] a, int[] b) {
         int count = 0;
         if (a.length != 0) {
-            if (a[0]== 1) count++; }
+            if (a[0] == 1) count++;
+        }
         if (b.length != 0) {
-            if (b[0]== 1) count++; }
+            if (b[0] == 1) count++;
+        }
 
         return count;
     }
 
-//    Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+    //    Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
     public int[] biggerTwo(int[] a, int[] b) {
         if (b[1] + b[0] > a[1] + a[0])
             return b;
@@ -631,13 +633,13 @@ public boolean commonEnd(int[] a, int[] b) {
             return a;
     }
 
-//    Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+    //    Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
     public int[] makeMiddle(int[] nums) {
         return new int[]{
-                nums[nums.length/2 -1], nums[nums.length/2]};
+                nums[nums.length / 2 - 1], nums[nums.length / 2]};
     }
 
-//    Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+    //    Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
     public int[] plusTwo(int[] a, int[] b) {
         int[] myArray = new int[4];
         myArray[0] = a[0];
@@ -648,37 +650,62 @@ public boolean commonEnd(int[] a, int[] b) {
         return myArray;
     }
 
-//    Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
+    //    Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
     public int[] swapEnds(int[] nums) {
         int a = nums[0];
         int b = nums[nums.length - 1];
         nums[0] = b;
-        nums[nums.length - 1] =a;
+        nums[nums.length - 1] = a;
         return nums;
     }
 
-//    Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array. The array length will be at least 3.
+    //    Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array. The array length will be at least 3.
     public int[] midThree(int[] nums) {
-        return new int[] {
-                nums[(nums.length+1)/2 -2],
-                nums[(nums.length+1)/2 -1],
-                nums[(nums.length+1)/2]};
+        return new int[]{
+                nums[(nums.length + 1) / 2 - 2],
+                nums[(nums.length + 1) / 2 - 1],
+                nums[(nums.length + 1) / 2]};
     }
 
-//    Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
-public int maxTriple(int[] nums) {
-    int result = 0;
-    int a = nums[0];
-    int b= nums[((nums.length+1)/2) -1];
-    int c = nums[nums.length -1];
+    //    Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+    public int maxTriple(int[] nums) {
+        int result = 0;
+        int a = nums[0];
+        int b = nums[((nums.length + 1) / 2) - 1];
+        int c = nums[nums.length - 1];
 
-    if (a>b && a>c)
-        result = a;
-    if (b>a && b>c)
-        result = b;
-    if (c>a && c>b)
-        result = c;
-    return result;
-}
+        if (a > b && a > c)
+            result = a;
+        if (b > a && b > c)
+            result = b;
+        if (c > a && c > b)
+            result = c;
+        return result;
+    }
+
+
+//    Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+    public int[] frontPiece(int[] nums) {
+        if (nums.length == 1)
+            return new int[]{nums[0]};
+        else if (nums.length == 0)
+            return new int[]{};
+        else
+            return new int[]{nums[0], nums[1]};
+    }
+
+//    We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+    public boolean unlucky1(int[] nums) {
+        if (nums.length ==0 || nums.length ==1)
+            return false;
+        else if ((nums[0] ==1 && nums[1] ==3) ||
+                (nums[1] ==1 && nums[2] ==3) ||
+                (nums[nums.length -1] ==3 &&
+                        nums[nums.length -2] ==1))
+            return true;
+        else
+            return false;
+    }
+
 
 }
