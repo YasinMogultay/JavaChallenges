20da44f9d3daaeaaf2eb254ff14566d3894c475b
Yasin Mogultay <yasin.mglty@gmail.com>
2021-10-01 21:46:32 -0500

    mapAB2 solved

--- CodingBat.java
+++ CodingBat.java
@@ -813,5 +813,19 @@ topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
         return map;
     }
 
-
+    /*
+    Modify and return the given map as follows: if the keys "a" and "b" are both in the map and have equal values, remove them both.
+    mapAB2({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
+    mapAB2({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
+    mapAB2({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
+    */
+    public Map<String, String> mapAB2(Map<String, String> map) {
+        String temp1 = map.get("a");
+        String temp2 = map.get("b");
+        if (temp1 != null && temp2 != null && temp1.equals(temp2)) {
+            map.remove("a");
+            map.remove("b");
+        }
+        return map;
+    }
 }
