cfaf073adc54d8e2829c8dc6b11dfd0ac03f9a79
Yasin Mogultay <yasin.mglty@gmail.com>
2021-10-06 19:16:21 -0500

    solved another map question

--- CodingBat.java
+++ CodingBat.java
@@ -844,4 +844,26 @@ topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
         }
         return map;
     }
+
+
+    /*
+    Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths, then set "c"
+    to have the longer value. If the values exist and have the same length, change them both to the empty string in the map.
+    */
+    public Map<String, String> mapAB4(Map<String, String> map) {
+
+        if (!map.containsKey("a") || !map.containsKey("b")) {
+            return map;
+        } else if (map.get("a").length() > map.get("b").length()) {
+            map.put("c", map.get("a"));
+        } else if (map.get("b").length() > map.get("a").length()) {
+            map.put("c", map.get("b"));
+        } else if (map.get("a").length() == map.get("b").length()) {
+            map.put("a", "");
+            map.put("b", "");
+        }
+
+        return map;
+
+    }
 }
