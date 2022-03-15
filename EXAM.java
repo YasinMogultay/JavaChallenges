b339b017e3de3203d382216a67e2cbfffe810ba8
Yasin Mogultay <yasin.mglty@gmail.com>
2022-03-15 15:58:17 -0500

    try

---
+++ EXAM.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EXAM {
+    public static void main(String[] args) {
+        boolean c="java".concat(" programming").trim().equals("java programming");
+        System.out.println(c);
+    }
+}
